1dd4895ca0dc408d678e259350113d004b368cb6
Yves YANG <yvyang>
2009-03-06 23:46:50 +0000

    add copyright header

--- bundles/org.eclipse.e4.xwt/src/org/eclipse/e4/xwt/input/CommandBinding.java
+++ bundles/org.eclipse.e4.xwt/src/org/eclipse/e4/xwt/input/CommandBinding.java
@@ -1,8 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2008 Soyatec (http://www.soyatec.com) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Soyatec - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.e4.xwt.input;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeSupport;
 
+/**
+ * 
+ * @author yyang (yves.yang@soyatec.com)
+ */
 public class CommandBinding {
 	protected PropertyChangeSupport canExecuteSupport = new PropertyChangeSupport(this);
 	protected PropertyChangeSupport executeSupport = new PropertyChangeSupport(this);
