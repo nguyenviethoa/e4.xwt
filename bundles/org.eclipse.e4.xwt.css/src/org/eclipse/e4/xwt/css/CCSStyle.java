75c494e3e068328c5562491ecbd0e9e6210f2a7d
Yves YANG <yvyang>
2009-03-07 11:55:41 +0000

    *** empty log message ***

--- bundles/org.eclipse.e4.xwt.css/src/org/eclipse/e4/xwt/css/CCSStyle.java
+++ bundles/org.eclipse.e4.xwt.css/src/org/eclipse/e4/xwt/css/CCSStyle.java
@@ -1,9 +1,49 @@
 package org.eclipse.e4.xwt.css;
 
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.e4.xwt.IStyle;
+import org.eclipse.e4.xwt.XWT;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 
 public class CCSStyle implements IStyle {
 	protected String url;
+
+	private Method applyStyles;
+	private Object engine;
+
+	public void initialize(Display display) {
+		// Instantiate SWT CSS Engine
+		try {
+			Class<?> engineClass = Class.forName("org.eclipse.e4.ui.css.nebula.engine.CSSNebulaEngineImpl"); //$NON-NLS-1$
+			Constructor<?> ctor = engineClass.getConstructor(new Class[] { Display.class, Boolean.TYPE });
+			engine = ctor.newInstance(new Object[] { display, Boolean.TRUE });
+			display.setData("org.eclipse.e4.ui.css.core.engine", engine); //$NON-NLS-1$
+
+			Class errorHandlerClass = Class.forName("org.eclipse.e4.ui.css.core.engine.CSSErrorHandler"); //$NON-NLS-1$
+			Method setErrorHandler = engineClass.getMethod("setErrorHandler", new Class[] { errorHandlerClass }); //$NON-NLS-1$
+			Class errorHandlerImplClass = Class.forName("org.eclipse.e4.ui.css.core.impl.engine.CSSErrorHandlerImpl"); //$NON-NLS-1$
+			setErrorHandler.invoke(engine, new Object[] { errorHandlerImplClass.newInstance() });
+
+			URL resolveUrl = FileLocator.resolve(new URL(url));
+			display.setData("org.eclipse.e4.ui.css.core.cssURL", resolveUrl); //$NON-NLS-1$		
+
+			InputStream stream = resolveUrl.openStream();
+			Method parseStyleSheet = engineClass.getMethod("parseStyleSheet", new Class[] { InputStream.class }); //$NON-NLS-1$
+			parseStyleSheet.invoke(engine, new Object[] { stream });
+			stream.close();
+
+			applyStyles = engineClass.getMethod("applyStyles", new Class[] { Object.class, Boolean.TYPE }); //$NON-NLS-1$
+		} catch (Throwable e) {
+			System.err.println("Warning - could not initialize CSS styling (but the applicationCSS property has a value) : " + e.toString()); //$NON-NLS-1$
+		}
+	}
 
 	public String getUrl() {
 		return url;
@@ -14,5 +54,24 @@ public class CCSStyle implements IStyle {
 	}
 
 	public void applyStyle(Object target) {
+		String name = XWT.getElementName(target);
+		Control control = null;
+		if (target instanceof Control) {
+			control = (Control) target;
+		} else if (target instanceof Viewer) {
+			Viewer viewer = (Viewer) target;
+			control = (Control) viewer.getControl();
+		}
+		if (control != null) {
+			//
+			// 
+			control.setData("org.eclipse.e4.ui.css.CssClassName", name);
+			control.setData("org.eclipse.e4.ui.css.id", name);
+			try {
+				applyStyles.invoke(engine, new Object[] { control, Boolean.FALSE });
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
