0c927afec2ea4153458a5b8e88ff0687fc45f221
Yves YANG <yvyang>
2009-03-31 04:06:29 +0000

    add XML data provider

---
+++ bundles/org.eclipse.e4.xwt.xml/src/org/eclipse/e4/xwt/xml/XmlObservableValue.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2008 Soyatec (http://www.soyatec.com) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Soyatec - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.e4.xwt.xml;
+
+import org.eclipse.e4.xwt.dataproviders.observable.XWTObservableValue;
+import org.w3c.dom.Node;
+
+/**
+ * @author jliu jin.liu@soyatec.com
+ */
+public class XmlObservableValue extends XWTObservableValue {
+
+	public XmlObservableValue(Object valueType, Node node) {
+		super(valueType, node);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.e4.xwt.dataproviders.xml.XWTObservableValue#doSetApprovedValue(java.lang.Object)
+	 */
+	protected void doSetApprovedValue(Object value) {
+		String newValue = value == null ? "" : value.toString();
+		((Node) getObserved()).setNodeValue(newValue);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.e4.xwt.dataproviders.xml.XWTObservableValue#doGetValue()
+	 */
+	protected Object doGetValue() {
+		return ((Node) getObserved()).getNodeValue();
+	}
+}
