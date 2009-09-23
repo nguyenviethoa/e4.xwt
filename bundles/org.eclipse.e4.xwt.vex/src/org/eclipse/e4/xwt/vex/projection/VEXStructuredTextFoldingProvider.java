819a743da5f52dd9e7fa2bb088f3c91ce4eef76e
Paul Webster <pwebster>
2009-09-23 13:06:34 +0000

    Bug 290174 -  Need to compile org.eclipse.e4.xwt.vex on 3.6 Remove the offending dependency

--- bundles/org.eclipse.e4.xwt.vex/src/org/eclipse/e4/xwt/vex/projection/VEXStructuredTextFoldingProvider.java
+++ bundles/org.eclipse.e4.xwt.vex/src/org/eclipse/e4/xwt/vex/projection/VEXStructuredTextFoldingProvider.java
@@ -10,11 +10,49 @@
  *******************************************************************************/
 package org.eclipse.e4.xwt.vex.projection;
 
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.source.projection.ProjectionViewer;
 import org.eclipse.wst.xml.ui.internal.projection.StructuredTextFoldingProviderXML;
 
-public class VEXStructuredTextFoldingProvider extends StructuredTextFoldingProviderXML {
+public class VEXStructuredTextFoldingProvider implements org.eclipse.wst.sse.ui.internal.projection.IStructuredTextFoldingProvider, org.eclipse.jface.text.source.projection.IProjectionListener, org.eclipse.jface.text.ITextInputListener { 
 
 	public VEXStructuredTextFoldingProvider() {
+	}
+
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void install(ProjectionViewer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void uninstall() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void projectionDisabled() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void projectionEnabled() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void inputDocumentAboutToBeChanged(IDocument oldInput,
+			IDocument newInput) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void inputDocumentChanged(IDocument oldInput, IDocument newInput) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
