25e2ddc253d6c917e12b5a771e807b13330f2537
Yves YANG <yvyang>
2009-10-11 11:25:02 +0000

    add shell transition

--- examples/org.eclipse.e4.demo.contacts/src/org/eclipse/e4/demo/contacts/handlers/ThemeUtil.java
+++ examples/org.eclipse.e4.demo.contacts/src/org/eclipse/e4/demo/contacts/handlers/ThemeUtil.java
@@ -22,11 +22,72 @@ import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.e4.ui.css.core.engine.CSSEngine;
 import org.eclipse.e4.workbench.ui.IWorkbench;
 import org.eclipse.e4.workbench.ui.internal.Workbench;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 public class ThemeUtil {
 
+	private static Shell initAnimation(Shell baseShell) {
+		GC gc = new GC(baseShell.getDisplay());
+		
+		// Ensure that the platform supports advanced graphics
+		gc.setAdvanced(true);
+		if (!gc.getAdvanced()) {
+			gc.dispose();
+			return null;
+		}
+		
+		Rectangle psRect = baseShell.getBounds();
+		Shell animationShell = new Shell(baseShell, SWT.NO_TRIM | SWT.ON_TOP);			
+		animationShell.setBounds(psRect);
+
+		// Capture the background image
+//		long startTime = System.currentTimeMillis();
+		Image backingStore = new Image(animationShell.getDisplay(), psRect);
+		gc.setAdvanced(true);
+		gc.copyArea(backingStore, psRect.x, psRect.y);
+		gc.dispose();
+		
+		animationShell.setAlpha(255);
+		animationShell.setBackgroundImage(backingStore);
+		animationShell.setVisible(true);
+//		System.out.println("Capture time = " + (System.currentTimeMillis()- startTime)); //$NON-NLS-1$
+		
+		return animationShell;
+	}
+	
+	private static void runAnimation(Shell animationShell) {
+		if (animationShell == null)
+			return;
+		
+		Rectangle ab = animationShell.getBounds();
+		long area = ab.width * ab.height;
+		
+		// Naive attempt to use fewer steps on large shells
+		int stepSize = 25;
+		if (area < 500000) stepSize = 10;
+		else if (area < 1000000) stepSize = 17;
+
+		
+		// 'Fade' the animation shell 
+		while (animationShell.getAlpha() > 0) {
+			int newAlpha = (int) (animationShell.getAlpha() - stepSize);
+			if (newAlpha < 0) newAlpha = 0;
+			animationShell.setAlpha(newAlpha);
+			animationShell.update();
+			
+			// Special safety-check, some platforms lie about supporting advanced graphics..;-)
+			if (animationShell.getAlpha() == 255)
+				break;  // it's broken so this makes sense...;-)
+		}
+		animationShell.getBackgroundImage().dispose();
+		animationShell.dispose();
+	}
+	
 	public static void switchTheme(IWorkbench workbench, final String css) {
 		if (workbench instanceof Workbench) {
 			Workbench wb = (Workbench) workbench;
@@ -42,6 +103,8 @@ public class ThemeUtil {
 								"platform:/plugin/org.eclipse.e4.demo.contacts/css/"
 										+ css));
 
+						Shell animationShell = initAnimation(shell);
+						
 						InputStream stream = url.openStream();
 						InputStreamReader streamReader = new InputStreamReader(
 								stream);
@@ -51,6 +114,8 @@ public class ThemeUtil {
 						streamReader.close();
 						engine.applyStyles(shell, true, false);
 						shell.layout(true, true);
+						
+						runAnimation(animationShell);
 					} catch (MalformedURLException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
