ba04b84186b28fb8736c9c14f08a5b0393660272
Yves YANG <yvyang>
2009-12-16 00:46:47 +0000

    add missing headers

--- bundles/org.eclipse.e4.xwt.pde/src/org/eclipse/e4/xwt/pde/ui/RCPApplication.java
+++ bundles/org.eclipse.e4.xwt.pde/src/org/eclipse/e4/xwt/pde/ui/RCPApplication.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2008 Soyatec (http://www.soyatec.com) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Soyatec - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.e4.xwt.pde.ui;
 
 import java.net.URL;
@@ -8,6 +18,12 @@ import org.eclipse.e4.xwt.XWT;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 
+/**
+ * Abstract application class for RCP
+ * 
+ * @author yyang
+ *
+ */
 public abstract class RCPApplication implements IApplication {
 
 	final public Object start(IApplicationContext context) throws Exception {
