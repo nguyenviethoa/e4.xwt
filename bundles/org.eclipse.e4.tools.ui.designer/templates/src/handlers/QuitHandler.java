3b7a3187d1be6aaef2e9b3867a5e537b196331ef
Oleg Besedin <obesedin>
2010-04-26 15:56:20 +0000

    Bug 307061 - Use annotations for invoke

--- bundles/org.eclipse.e4.tools.ui.designer/templates/src/handlers/QuitHandler.java
+++ bundles/org.eclipse.e4.tools.ui.designer/templates/src/handlers/QuitHandler.java
@@ -5,12 +5,14 @@ import java.lang.reflect.InvocationTargetException;
 import javax.inject.Named;
 
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.e4.workbench.ui.IWorkbench;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 
 public class QuitHandler {
+	@Execute
 	public void execute(IWorkbench workbench, IEclipseContext context,
 			@Named(IServiceConstants.ACTIVE_SHELL) Shell shell)
 			throws InvocationTargetException, InterruptedException {
--- bundles/org.eclipse.e4.tools.ui.designer/templates/src/handlers/SaveHandler.java
+++ bundles/org.eclipse.e4.tools.ui.designer/templates/src/handlers/SaveHandler.java
@@ -9,6 +9,8 @@ import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.EclipseContextFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.IDisposable;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.ui.model.application.MContribution;
 import org.eclipse.e4.ui.model.application.ui.MDirtyable;
 import org.eclipse.e4.ui.services.IServiceConstants;
@@ -17,6 +19,7 @@ import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.widgets.Shell;
 
 public class SaveHandler {
+	@CanExecute
 	public boolean canExecute(
 			@Named(IServiceConstants.ACTIVE_PART) MDirtyable dirtyable) {
 		if (dirtyable == null) {
@@ -25,6 +28,7 @@ public class SaveHandler {
 		return dirtyable.isDirty();
 	}
 
+	@Execute
 	public void execute(
 			IEclipseContext context,
 			@Named(IServiceConstants.ACTIVE_SHELL) Shell shell,
