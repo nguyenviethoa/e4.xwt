7528c85495445b24436f0cb4ad5130aa69a3b509
Yves YANG <yvyang>
2010-06-17 22:29:25 +0000

    Bug 316941 - org.eclipse.e4.tools.ui.designer is known to not run on OS-X

--- bundles/org.eclipse.e4.tools.ui.designer/src/org/eclipse/e4/tools/ui/designer/E4WorkbenchProxy.java
+++ bundles/org.eclipse.e4.tools.ui.designer/src/org/eclipse/e4/tools/ui/designer/E4WorkbenchProxy.java
@@ -10,6 +10,31 @@
  *******************************************************************************/
 package org.eclipse.e4.tools.ui.designer;
 
+import org.eclipse.e4.ui.internal.workbench.swt.ResourceUtility;
+import org.eclipse.e4.ui.workbench.IResourceUtilities;
+
+import org.eclipse.e4.ui.internal.workbench.ToolBarContributionHandler;
+import org.eclipse.e4.ui.internal.workbench.TrimContributionHandler;
+import org.eclipse.e4.ui.workbench.swt.modeling.MenuServiceFilter;
+
+import org.eclipse.e4.ui.css.swt.engine.CSSSWTEngineImpl;
+
+import org.eclipse.e4.ui.css.swt.theme.IThemeManager;
+
+import org.eclipse.e4.ui.css.swt.theme.IThemeEngine;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.e4.ui.css.core.util.impl.resources.OSGiResourceLocator;
+import org.eclipse.e4.ui.internal.workbench.swt.E4Application;
+import org.eclipse.e4.ui.workbench.swt.WorkbenchSWTActivator;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
 import org.eclipse.e4.ui.internal.workbench.ModelAssembler;
 
 import java.util.ArrayList;
@@ -46,6 +71,7 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Widget;
 
 /**
+ * Mainly comes from PartRenderingEngine
  * @author jin.liu(jin.liu@soyatec.com)
  */
 public class E4WorkbenchProxy {
@@ -55,6 +81,8 @@ public class E4WorkbenchProxy {
 	private Object root;
 	private E4UIEventPublisher globalDistahcher;
 	private MApplicationElement uiRoot;
+	private MenuServiceFilter menuServiceFilter;
+	org.eclipse.swt.widgets.Listener keyListener;
 
 	public E4WorkbenchProxy(MApplicationElement uiRoot,
 			IEclipseContext applicationContext) {
@@ -124,6 +152,12 @@ public class E4WorkbenchProxy {
 		Realm.runWithDefault(SWTObservables.getRealm(display), new Runnable() {
 
 			public void run() {
+				initializeStyling(display, appContext);
+
+				// Register an SWT resource handler
+				appContext.set(IResourceUtilities.class.getName(),
+						new ResourceUtility());
+
 				// Has someone already created one ?
 				renderer = (IPresentationEngine) appContext
 						.get(IPresentationEngine.class.getName());
@@ -146,15 +180,15 @@ public class E4WorkbenchProxy {
 					}
 				}
 
-				String cssURI = (String) appContext
-						.get(E4Workbench.CSS_URI_ARG);
-				if (cssURI != null) {
-					String cssResourcesURI = (String) appContext
-							.get(E4Workbench.CSS_RESOURCE_URI_ARG);
-					PartRenderingEngine.initializeStyling(display, appContext);
-				} else {
-					initializeNullStyling(appContext);
-				}
+//				String cssURI = (String) appContext
+//						.get(E4Workbench.CSS_URI_ARG);
+//				if (cssURI != null) {
+//					String cssResourcesURI = (String) appContext
+//							.get(E4Workbench.CSS_RESOURCE_URI_ARG);
+//					PartRenderingEngine.initializeStyling(display, appContext);
+//				} else {
+//					initializeNullStyling(appContext);
+//				}
 
 				// Register an SWT resource handler
 				// appContext.set(IResourceUtilities.class.getName(), new
@@ -164,10 +198,20 @@ public class E4WorkbenchProxy {
 				// set up the keybinding manager
 				KeyBindingDispatcher dispatcher = (KeyBindingDispatcher) ContextInjectionFactory
 						.make(KeyBindingDispatcher.class, appContext);
-				org.eclipse.swt.widgets.Listener listener = dispatcher
-						.getKeyDownFilter();
-				display.addFilter(SWT.KeyDown, listener);
-				display.addFilter(SWT.Traverse, listener);
+				keyListener = dispatcher.getKeyDownFilter();
+				display.addFilter(SWT.KeyDown, keyListener);
+				display.addFilter(SWT.Traverse, keyListener);
+
+				ContextInjectionFactory.make(TrimContributionHandler.class,
+						appContext);
+				ContextInjectionFactory.make(ToolBarContributionHandler.class,
+						appContext);
+				menuServiceFilter = ContextInjectionFactory.make(
+						MenuServiceFilter.class, appContext);
+				display.addFilter(SWT.Show, menuServiceFilter);
+				display.addFilter(SWT.Hide, menuServiceFilter);
+				display.addFilter(SWT.Dispose, menuServiceFilter);
+				appContext.set(MenuServiceFilter.class, menuServiceFilter);
 
 				// if (device == null || device.isDisposed()) {
 				// device = new Shell(display, SWT.NO_TRIM);
@@ -208,6 +252,120 @@ public class E4WorkbenchProxy {
 			}
 		});
 	}
+	
+	public static void initializeStyling(Display display,
+			IEclipseContext appContext) {
+		String cssTheme = (String) appContext.get(E4Application.THEME_ID);
+		String cssURI = (String) appContext.get(E4Workbench.CSS_URI_ARG);
+
+		if (cssTheme != null) {
+			String cssResourcesURI = (String) appContext
+					.get(E4Workbench.CSS_RESOURCE_URI_ARG);
+
+			Bundle bundle = WorkbenchSWTActivator.getDefault().getBundle();
+			BundleContext context = bundle.getBundleContext();
+			ServiceReference ref = context
+					.getServiceReference(org.eclipse.ui.themes.IThemeManager.class.getName());
+			IThemeManager mgr = (IThemeManager) context.getService(ref);
+			final IThemeEngine engine = mgr.getEngineForDisplay(display);
+
+			// Store the app context
+			display.setData("org.eclipse.e4.ui.css.context", appContext); //$NON-NLS-1$
+
+			// Create the OSGi resource locator
+			if (cssResourcesURI != null) {
+				// TODO: Should this be set through an extension as well?
+				engine.registerResourceLocator(new OSGiResourceLocator(
+						cssResourcesURI));
+			}
+
+			engine.restore(cssTheme);
+			// TODO Should we create an empty default theme?
+
+			appContext.set(IThemeEngine.class.getName(), engine);
+
+			appContext.set(IStylingEngine.SERVICE_NAME, new IStylingEngine() {
+				public void setClassname(Object widget, String classname) {
+					((Widget) widget).setData(
+							"org.eclipse.e4.ui.css.CssClassName", classname); //$NON-NLS-1$
+					engine.applyStyles((Widget) widget, true);
+				}
+
+				public void setId(Object widget, String id) {
+					((Widget) widget).setData("org.eclipse.e4.ui.css.id", id); //$NON-NLS-1$
+					engine.applyStyles((Widget) widget, true);
+				}
+
+				public void style(Object widget) {
+					engine.applyStyles((Widget) widget, true);
+				}
+
+			});
+		} else if (cssURI != null) {
+			String cssResourcesURI = (String) appContext
+					.get(E4Workbench.CSS_RESOURCE_URI_ARG);
+			final CSSSWTEngineImpl engine = new CSSSWTEngineImpl(display, true);
+			if (cssResourcesURI != null) {
+				engine.getResourcesLocatorManager().registerResourceLocator(
+						new OSGiResourceLocator(cssResourcesURI.toString()));
+			}
+			appContext.set(IStylingEngine.SERVICE_NAME, new IStylingEngine() {
+				public void setClassname(Object widget, String classname) {
+					((Widget) widget).setData(
+							"org.eclipse.e4.ui.css.CssClassName", classname); //$NON-NLS-1$
+					engine.applyStyles((Widget) widget, true);
+				}
+
+				public void setId(Object widget, String id) {
+					((Widget) widget).setData("org.eclipse.e4.ui.css.id", id); //$NON-NLS-1$
+					engine.applyStyles((Widget) widget, true);
+				}
+
+				public void style(Object widget) {
+					engine.applyStyles((Widget) widget, true);
+				}
+
+			});
+
+			URL url;
+			InputStream stream = null;
+			try {
+				url = FileLocator.resolve(new URL(cssURI));
+				stream = url.openStream();
+				engine.parseStyleSheet(stream);
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				if (stream != null) {
+					try {
+						stream.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+
+			Shell[] shells = display.getShells();
+			for (Shell s : shells) {
+				try {
+					s.setRedraw(false);
+					s.reskin(SWT.ALL);
+					engine.applyStyles(s, true);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} finally {
+					s.setRedraw(true);
+				}
+			}
+		}
+
+	}
 
 	public Object create(MUIElement element) {
 		if (renderer != null) {
@@ -222,6 +380,33 @@ public class E4WorkbenchProxy {
 		}
 	}
 
+	/*
+	 * There are situations where this is called more than once until we know
+	 * why this is needed we should make this safe for multiple calls
+	 */
+	private void cleanUp() {
+		if (menuServiceFilter != null) {
+			Display display = Display.getDefault();
+			if (!display.isDisposed()) {
+				display.removeFilter(SWT.Show, menuServiceFilter);
+				display.removeFilter(SWT.Hide, menuServiceFilter);
+				display.removeFilter(SWT.Dispose, menuServiceFilter);
+				menuServiceFilter.dispose();
+				menuServiceFilter = null;
+				appContext.remove(MenuServiceFilter.class);
+			}
+		}
+		if (keyListener != null) {
+			Display display = Display.getDefault();
+			if (!display.isDisposed()) {
+				display.removeFilter(SWT.KeyDown, keyListener);
+				display.removeFilter(SWT.Traverse, keyListener);
+				keyListener = null;
+			}
+		}
+	}
+
+	
 	protected void initializeNullStyling(IEclipseContext appContext) {
 		appContext.set(IStylingEngine.SERVICE_NAME, new IStylingEngine() {
 			public void setClassname(Object widget, String classname) {
